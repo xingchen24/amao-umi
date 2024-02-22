cd471fbacd1bd6e6aba9a35bf7d4a60114bad9a8
xingchen24 <cx1801126003@gmail.com>
2024-02-22 10:03:09 +0800

    test git cx

---
+++ .history/src/pages/learnTs/utils/interfaceTest_20240222100044.ts
@@ -0,0 +1,20 @@
+interface SquareConfig {
+  color?: string;
+  width?: number;
+}
+
+//interface的函数写法
+interface SearchFunc {
+  (source: string, subString: string): boolean;
+}
+//interface的索引类型写法
+interface StringArray {
+  [index: number]: string
+}
+//interface中所有的成员都必须满足索引类型
+interface StringTest {
+  length: number;
+  name: number;
+  [index: string]: number;
+}
+//interface中继承多个接口
